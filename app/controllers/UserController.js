f32982d0790340a8659f89e0ef5dee097315025b
Namibo06 <namibo654@gmail.com>
2024-08-13 14:27:24 -0300

    refactor: abstracted somes validations and added the method updateUser

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -38,7 +38,9 @@ exports.createUser = async (req,res) => {
     const phone = req.body.phone;
     const password = req.body.password;
 
-    validateRequest(res,firstName,lastName,email,phone,password);
+    validateRequestEmptyInCreate(res,firstName,email,password);
+    validateEmail(res,email);
+    validateRequestLengthInCreate(res,firstName,lastName,phone,password);
 
     try{
         const user = await User.create(req.body);
@@ -48,11 +50,40 @@ exports.createUser = async (req,res) => {
     }
 };
 
-function validateRequest(res,firstName,lastName,email,phone,password){
-    if(validator.isEmpty(firstName) || validator.isEmpty(email) || validator.isEmpty(password)){
-        return res.status(400).json({message:"Campos obrigatórios vazios"});
-    }
+exports.updateUser = async (req,res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const email = req.body.email;
+    const phone = req.body.phone;
 
+    validateEmail(res,email);
+    validateRequestLength(res,firstName,lastName,phone);
+    validateRequestEmptyInUpdate(res,firstName,email);
+
+    try{
+        const [updated] = await User.update(req.body,{
+            where:{
+                id:req.params.uuid
+            }
+        });
+
+        if(!updated){
+            return res.status(400).json({message:"Houve falha ao tentar atualizar"});
+        }
+
+        return res.status(200).json({message: "Usuário atualizado com sucesso"});
+    }catch(error){
+        return res.status(500).json({message: "Erro: " + error});
+    }
+};
+
+function validateEmail(res,email){
+    if(!validator.isEmail(email)){
+        return res.status(400).json({message: "Email inválido"});
+    }
+}
+
+function validateRequestLength(res,firstName,lastName,phone){
     if(firstName.length < 3 || firstName > 15){
         return res.status(400).json({message:"Primeiro nome deve conter entre 3 e 15 caracteres"});
     }
@@ -63,17 +94,29 @@ function validateRequest(res,firstName,lastName,email,phone,password){
         }
     }
 
-    if(!validator.isEmail(email)){
-        return res.status(400).json({message: "Email inválido"});
-    }
-
     if(!validator.isEmpty(phone)){
         if(phone.length < 11 || phone.length > 11){
             return res.status(400).json({message: "Telefone deve conter 11 caracteres no formato 75990000000"});
         }
     }
+}
+
+function validateRequestLengthInCreate(res,firstName,lastName,phone,password){
+    validateRequestLength(res,firstName,lastName,phone,password);
 
     if(password.length < 8 || password.length > 32){
         return res.status(400).json({message: "Senha deve conter entre 8 e 32 caracteres"});
+    }
+}
+
+function validateRequestEmptyInCreate(res,firstName,email,password){
+    if(validator.isEmpty(firstName) || validator.isEmpty(email) || validator.isEmpty(password)){
+        return res.status(400).json({message:"Campos obrigatórios vazios"});
+    }
+}
+
+function validateRequestEmptyInUpdate(res,firstName,email){
+    if(validator.isEmpty(firstName) || validator.isEmpty(email)){
+        return res.status(400).json({message:"Campos obrigatórios vazios"});
     }
 }
