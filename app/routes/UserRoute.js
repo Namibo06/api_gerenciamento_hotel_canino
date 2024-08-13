0c6344502bffdad66a431c6c7d0b8af2eb16be38
Namibo06 <namibo654@gmail.com>
2024-08-13 13:54:01 -0300

    refactor: add new route

--- app/routes/UserRoute.js
+++ app/routes/UserRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 
 router.get('/',UserController.listUsers);
+router.get('/:uuid',UserController.getUserByUUID);
 router.post('/',UserController.createUser);
 
 module.exports = router;
