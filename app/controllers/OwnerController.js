f1271972781e2126d7bcec15ee662fd4165608e3
Namibo06 <namibo654@gmail.com>
2024-08-14 12:30:43 -0300

    refactor: remove validation of controller && create two functions

--- app/controllers/OwnerController.js
+++ app/controllers/OwnerController.js
@@ -1,6 +1,8 @@
 const Owner = require('../models/Owner');
-const validator = require('validator');
-const { validateEmail, validateRequestLength, validateRequestEmptyInUpdate } = require('../controllers/UserController');
+const { validateEmail } = require('../utils/validationEmail');
+const { validateFormatUUID } = require('../utils/validationUUID');
+const { validateLength } = require('../utils/validationLength');
+const { validateEmpty } = require('../utils/validationEmpty');
 
 exports.listOwners = async (req,res) => {
     try{
@@ -13,6 +15,19 @@ exports.listOwners = async (req,res) => {
         return res.status(200).json({owners: owners});
     }catch(error){
         return res.status(500).json({message: "Erro: " + error});
+    }
+};
+
+exports.getOwnerByUUID = async (req,res) => {
+    const uuid = req.params.uuid;
+    validateFormatUUID(res,uuid);
+    if (res.headersSent) return;
+
+    try{    
+        const owner = await Owner.findByPk(uuid);
+        return res.status(200).json({owner: owner});
+    }catch(error){
+        console.log("Erro: " + error);
     }
 };
 
@@ -38,21 +53,38 @@ exports.createOwner = async (req,res) => {
     }
 };
 
+exports.updateOwner = async (req,res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const cpf = req.body.cpf;
+    const email = req.body.email;
+    const phone = req.body.phone;
+
+    validateEmail(res,email);
+    if (res.headersSent) return;
+    validateLength(res,firstName,lastName,cpf,phone);
+    if (res.headersSent) return;
+    validateEmpty(res,firstName,email,phone,cpf);
+    if (res.headersSent) return;
+
+    try{
+        const [updated] = await Owner.update(req.body,{
+            where:{
+                id:req.params.uuid
+            }
+        });
+    
+        if(!updated){
+            return res.status(500).json({message: "Falha ao tentar atualizar"});
+        }
+
+        return res.status(200).json({message: "Atualizado com sucesso"});
+    }catch(error){
+        console.log("Erro: " + error);
+    }
+
+};
+
 /**validations */
-function validateLength(res,firstName,lastName,cpf,phone){
-    validateRequestLength(res,firstName,lastName,phone);
-    if (res.headersSent) return;
 
-    if(cpf.length > 11 || cpf.length < 11){
-        return res.status(400).json({message: "CPF deve conter 11 caracteres"});
-    }
-}
 
-function validateEmpty(res,firstName,email,phone,cpf){
-    validateRequestEmptyInUpdate(res,firstName,email);
-    if (res.headersSent) return;
-
-    if(validator.isEmpty(phone) || validator.isEmpty(cpf)){
-        return res.status(400).json({message:"Campos obrigatórios vazios"});
-    }
-}
