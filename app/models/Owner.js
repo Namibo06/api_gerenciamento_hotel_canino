0f7eb0ac49be680e1861bb42366b46220cbb71b3
Namibo06 <namibo654@gmail.com>
2024-08-14 02:02:15 -0300

    feat: model owner

---
+++ app/models/Owner.js
@@ -0,0 +1,34 @@
+const { Sequelize,DataTypes, UUIDV4 } = require('sequelize');
+const sequelize = require('../config/Database');
+
+const owner = sequelize.define('tb_owner',{
+    id:{
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: UUIDV4
+    },
+    first_name:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    cpf:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    email:{
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    phone:{
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = owner;
