6a714b8690064e08abec1f5c8f4e521daa6f3241
Namibo06 <namibo654@gmail.com>
2024-08-14 12:29:55 -0300

    feat: add new routes

--- app/routes/OwnerRoute.js
+++ app/routes/OwnerRoute.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const OwnerController = require('../controllers/OwnerController');
 
 router.get('/',OwnerController.listOwners);
+router.get('/:uuid',OwnerController.getOwnerByUUID);
 router.post('/',OwnerController.createOwner);
+router.patch('/:uuid',OwnerController.updateOwner);
 
 
 module.exports = router;
