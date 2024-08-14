8dfa99a7db95ebc3bd8d6d2ccfbaf77ea54ea96c
Namibo06 <namibo654@gmail.com>
2024-08-14 15:47:18 -0300

    feat: listDogs and createDog

---
+++ app/controllers/DogController.js
@@ -0,0 +1,64 @@
+const Dog = require('../models/Dog');
+const { validationYearDog } = require('../utils/validationYearDog');
+const { validationPostage } = require('../utils/validationPostage');
+const { validateFormatUUID } = require('../utils/validationUUID');
+const { validateNameDogLength } = require('../utils/validationDogLength');
+const Owner = require('../models/Owner');
+
+exports.listDogs = async (req,res) => {
+    try{
+        const dogs = await Dog.findAll();
+
+        if(dogs.length === 0){
+            return res.status(404).json({message: "Nenhum registro encontrado"});
+        }
+
+        return res.status(200).json({dogs: dogs});
+    }catch(error){
+        console.log("Erro: " + error);
+    }
+}
+
+exports.createDog = async (req,res) => {
+    try{
+        const name = req.body.name;
+        const year = req.body.year;
+        const color = req.body.color;
+        const postage = req.body.postage;
+        const race = req.body.race;
+        const restrictions = req.body.restrictions;
+        const deficiency = req.body.deficiency;
+        const owners = req.body.owners;
+
+        validationYearDog(res,year);
+        if (res.headersSent) return;
+        validationPostage(res,postage);
+        if (res.headersSent) return;
+        validateNameDogLength(res,name);
+        if (res.headersSent) return;
+
+        let listOwner = [];
+        for (let i = 0; i < owners.length; i++) {
+            let ownerId = owners[i];
+            validateFormatUUID(res,ownerId);
+            if (res.headersSent) return;
+            let ownerExists = await Owner.findByPk(ownerId);
+
+            if(!ownerExists){
+                listOwner.push(ownerId);
+                console.log(listOwner);
+            }
+        }
+
+        if (listOwner.length > 0) {
+            return res.status(400).json({
+                error: `Os seguintes IDs de donos são inválidos: ${listOwner.join(', ')}`
+            });
+        }
+
+        const dog = await Dog.create(req.body);
+        return res.status(201).json({dog: dog});
+    }catch(error){
+        console.log("Erro: " + error);
+    }
+}
