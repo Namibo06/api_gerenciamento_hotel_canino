960bc7461b2a4aa6ebd31730aecd4876a5a329e0
Namibo06 <namibo654@gmail.com>
2024-08-15 14:11:53 -0300

    feat: add new routes

--- app/routes/DogRoute.js
+++ app/routes/DogRoute.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const DogController = require('../controllers/DogController');
 
 router.get('/',DogController.listDogs);
+router.get('/:uuid',DogController.getDogByUUID);
 router.post('/',DogController.createDog);
+router.put('/:uuid',DogController.updateDog);
+router.delete('/:uuid',DogController.deleteDog);
 
 module.exports = router;
