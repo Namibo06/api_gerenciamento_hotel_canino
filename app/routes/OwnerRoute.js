c021881a0863739366e87edeca74855c6cf98aab
Namibo06 <namibo654@gmail.com>
2024-08-16 01:02:07 -0300

    refactor: add req and res

--- app/routes/OwnerRoute.js
+++ app/routes/OwnerRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const OwnerController = require('../controllers/OwnerController');
+const router = express.Router();
+const ownerController = new OwnerController();
 
-router.get('/',OwnerController.listOwners);
-router.get('/:uuid',OwnerController.getOwnerByUUID);
-router.post('/',OwnerController.createOwner);
-router.put('/:uuid',OwnerController.updateOwner);
-router.delete('/:uuid',OwnerController.deleteUser);
+router.get('/',(req,res) => ownerController.listOwners(req,res));
+router.get('/:uuid',(req,res) => ownerController.getOwnerByUUID(req,res));
+router.post('/',(req,res) => ownerController.createOwner(req,res));
+router.put('/:uuid',(req,res) => ownerController.updateOwner(req,res));
+router.delete('/:uuid',(req,res) => ownerController.deleteUser(req,res));
 
 
 module.exports = router;
