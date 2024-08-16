5574aaecb82e038de211af22d0e7757fa47f853c
Namibo06 <namibo654@gmail.com>
2024-08-16 01:04:15 -0300

    refactor: remove model and add usecase in controller

--- app/controllers/OwnerController.js
+++ app/controllers/OwnerController.js
@@ -1,112 +1,55 @@
-const Owner = require('../models/Owner');
-const { validateEmail } = require('../utils/validationEmail');
-const { validateFormatUUID } = require('../utils/validationUUID');
-const { validateLength } = require('../utils/validationLength');
-const { validateEmpty } = require('../utils/validationEmpty');
+const OwnerUseCase = require('../usecases/OwnerUseCase');
 
-exports.listOwners = async (req,res) => {
-    try{
-        const owners = await Owner.findAll();
-
-        if(owners.length === 0){
-            return res.status(404).json({message: "Nenhum  registro encontrado"});    
-        }
-
-        return res.status(200).json({owners: owners});
-    }catch(error){
-        return res.status(500).json({message: "Erro: " + error});
+module.exports = class OwnerController{
+    constructor(){
+        this.OwnerUseCase = new OwnerUseCase();
     }
-};
 
-exports.getOwnerByUUID = async (req,res) => {
-    const uuid = req.params.uuid;
-    validateFormatUUID(res,uuid);
-    if (res.headersSent) return;
-
-    try{    
-        const owner = await Owner.findByPk(uuid);
-        return res.status(200).json({owner: owner});
-    }catch(error){
-        console.log("Erro: " + error);
-    }
-};
-
-exports.createOwner = async (req,res) => {
-    const firstName = req.body.first_name;
-    const lastName = req.body.last_name;
-    const cpf = req.body.cpf;
-    const email = req.body.email;
-    const phone = req.body.phone;
-
-    validateEmail(res,email);
-    if (res.headersSent) return;
-    validateLength(res,firstName,lastName,cpf,phone);
-    if (res.headersSent) return;
-    validateEmpty(res,firstName,email,phone,cpf);
-    if (res.headersSent) return;
-
-    try{
-        const owner = await Owner.create(req.body);
-        return res.status(201).json({message: "Dono cadastrado com sucesso",owner: owner});
-    }catch(error){
-        console.log("Erro: " + error);
-    }
-};
-
-exports.updateOwner = async (req,res) => {
-    const firstName = req.body.first_name;
-    const lastName = req.body.last_name;
-    const cpf = req.body.cpf;
-    const email = req.body.email;
-    const phone = req.body.phone;
-
-    const uuid = req.params.uuid;
-    validateFormatUUID(res,uuid);
-    if (res.headersSent) return;
-
-    validateEmail(res,email);
-    if (res.headersSent) return;
-    validateLength(res,firstName,lastName,cpf,phone);
-    if (res.headersSent) return;
-    validateEmpty(res,firstName,email,phone,cpf);
-    if (res.headersSent) return;
-
-    try{
-        const [updated] = await Owner.update(req.body,{
-            where:{
-                id:uuid
-            }
-        });
+    async listOwners(req,res) {
+        try{
+            const owners = await this.OwnerUseCase.findAllOwners(res);
     
-        if(!updated){
-            return res.status(500).json({message: "Falha ao tentar atualizar"});
+            return res.status(200).json({owners: owners});
+        }catch(error){
+            console.log("Erro: " + error);
         }
-
-        return res.status(200).json({message: "Atualizado com sucesso"});
-    }catch(error){
-        console.log("Erro: " + error);
-    }
-
-};
-
-exports.deleteUser = async (req,res) => {
-    const uuid = req.params.uuid;
-    validateFormatUUID(res,uuid);
-    if(res.headersSent) return;
-
-    try{
-        const owner = await Owner.destroy({
-            where:{
-                id:uuid
-            }
-        });
-
-        if(!owner){
-            return res.status(404).json({message: "Dono não encontrado"});
+    };
+    
+    async getOwnerByUUID(req,res) {
+        try{    
+            const owner = await this.OwnerUseCase.getOwnerByUUID(req,res);
+            return res.status(200).json({owner: owner});
+        }catch(error){
+            console.log("Erro: " + error);
         }
-
-        return res.status(204).send();
-    }catch(error){
-        console.log("Erro: " + error);
-    }
-};
+    };
+    
+    async createOwner(req,res) {
+        try{
+            const owner = await this.OwnerUseCase.createOwner(req,res);
+            return res.status(201).json({message: "Dono cadastrado com sucesso",owner: owner});
+        }catch(error){
+            console.log("Erro: " + error);
+        }
+    };
+    
+    async updateOwner(req,res) {
+        try{
+            await this.OwnerUseCase.updateOwner(req,res);
+    
+            return res.status(200).json({message: "Atualizado com sucesso"});
+        }catch(error){
+            console.log("Erro: " + error);
+        }
+    };
+    
+    async deleteUser(req,res) {
+        try{
+            await this.OwnerUseCase.deleteOwner(req,res);
+    
+            return res.status(204).send();
+        }catch(error){
+            console.log("Erro: " + error);
+        }
+    };
+}
