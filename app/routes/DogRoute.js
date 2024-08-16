15782604d9d3cae2b9c6ab61e4e8da67371180bb
Namibo06 <namibo654@gmail.com>
2024-08-16 02:09:35 -0300

    refactor: add req and res

--- app/routes/DogRoute.js
+++ app/routes/DogRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const DogController = require('../controllers/DogController');
+const dogController = new DogController();
 
-router.get('/',DogController.listDogs);
-router.get('/:uuid',DogController.getDogByUUID);
-router.post('/',DogController.createDog);
-router.put('/:uuid',DogController.updateDog);
-router.delete('/:uuid',DogController.deleteDog);
+router.get('/',(req,res) => dogController.listDogs(req,res));
+router.get('/:uuid',(req,res) => dogController.getDogByUUID(req,res));
+router.post('/',(req,res) => dogController.createDog(req,res));
+router.put('/:uuid',(req,res) => dogController.updateDog(req,res));
+router.delete('/:uuid',(req,res) => dogController.deleteDog(req,res));
 
 module.exports = router;
