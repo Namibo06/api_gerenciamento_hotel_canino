f8aefad7ac212a9443d2c2151ea67ad36c3584a6
Namibo06 <namibo654@gmail.com>
2024-08-16 23:10:45 -0300

    feat: method verifyToken and change validation in authenticate

--- app/usecases/LoginUseCase.js
+++ app/usecases/LoginUseCase.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const UserService = require('../services/UserService');
 const jwt = require('jsonwebtoken');
-const validateEmail = require('../utils/validationEmail');
+const { validateEmail } = require('../utils/validationEmail');
+const bcrypt = require('bcrypt');
 
 module.exports = class LoginUseCase{
     constructor(){
@@ -14,16 +15,43 @@ module.exports = class LoginUseCase{
         validateEmail(res,email);
         if(res.headersSent) return;
 
-        const login = await this.UserService.findByEmailAndPassword(email,password);
+        const login = await this.UserService.findByEmail(email);
 
         if(!login){
             return res.status(404).json({message: "Usuário não encontrado"});
         }
+
+        const validatePassword = await bcrypt.compare(password,login.password);
+
+        if(validatePassword === false){
+            return res.status(400).json({message: "Senhas não batem"});
+        }
         
-        const token = jwt.sign(email,process.env.TOKEN_SECRET,{ expiresIn: '259200' }); //3 dias
+        const token = jwt.sign({ email },process.env.TOKEN_SECRET,{ expiresIn: '3d' }); //3 dias
 
         await this.UserService.updateToken(token,email);
 
         return token;
+    }
+
+    async verifyToken(authHeader){
+        if (!authHeader) {
+            throw new Error("Não autorizado");
+        }
+    
+        const parts = authHeader.split(' ');
+
+        if (parts.length !== 3 || parts[1] !== 'Bearer') {
+            throw new Error("Não autorizado");
+        }
+    
+        const token = parts[2];
+    
+        try {
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+            return decoded; 
+        } catch (err) {
+            throw new Error('Token inválido');
+        }
     }
 }
