44632887002832e6c546f0a5267821b45d1299de
Namibo06 <namibo654@gmail.com>
2024-08-16 23:38:22 -0300

    feat: added new route

--- app/routes/UserRoute.js
+++ app/routes/UserRoute.js
@@ -7,6 +7,7 @@ router.get('/',(req,res) => userController.listUsers(req,res));
 router.get('/:uuid', (req,res) => userController.getUserByUUID(req,res));
 router.post('/',(req,res) => userController.createUser(req,res));
 router.patch('/:uuid',(req,res) => userController.updateUser(req,res));
+router.patch('/updatePassword/:uuid',(req,res) => userController.updatePassword(req,res));
 router.delete('/:uuid',(req,res) => userController.deleteUser(req,res));
 
 module.exports = router;
