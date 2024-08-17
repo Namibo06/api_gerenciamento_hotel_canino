3623cac5e98ca721cf0bcb53d28d25a6192e30c2
Namibo06 <namibo654@gmail.com>
2024-08-16 23:10:04 -0300

    feat: added new route for verify token

--- app/routes/LoginRoute.js
+++ app/routes/LoginRoute.js
@@ -4,5 +4,6 @@ const LoginController = require('../controllers/LoginController');
 const loginController = new LoginController();
 
 router.post('/',(req,res) => loginController.authenticate(req,res));
+router.post('/verifyToken',(req,res) => loginController.verifyToken(req,res));
 
 module.exports = router;
