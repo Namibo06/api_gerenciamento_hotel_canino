4a7deb1b2471ce12dc6d00bff0c0a15caf60d9e3
Namibo06 <namibo654@gmail.com>
2024-08-18 20:52:52 -0300

    feat: configure swagger

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const express = require('express');
-const { connectDB,sequelize } = require('./app/config/Database');
-const UserRoute = require('./app/routes/UserRoute');
-const OwnerRoute = require('./app/routes/OwnerRoute');
-const DogRoute = require('./app/routes/DogRoute');
-const LoginRoute = require('./app/routes/LoginRoute');
-const owner = require('./app/models/Owner');
-const dog = require('./app/models/Dog');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const { connectDB,sequelize } = require('./app/infrastructure/config/Database');
+const UserRoute = require('./app/application/routes/UserRoute');
+const OwnerRoute = require('./app/application/routes/OwnerRoute');
+const DogRoute = require('./app/application/routes/DogRoute');
+const LoginRoute = require('./app/application/routes/LoginRoute');
+const owner = require('./app/domain/models/Owner');
+const dog = require('./app/domain/models/Dog');
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 connectDB();
 
-app.use(express.json());
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Gerenciador Hotel Canino',
+            version: '1.0.0',
+            description: 'API RESTFull com Node.js e Express'
+        },
+        serves: [
+            {
+                url: 'http://localhost:3000'
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
+        ]
+    },
+    apis: ['./app/application/controllers/*.js']
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+
+app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerDocs));
 app.use('/api/users',UserRoute);
 app.use('/api/owners',OwnerRoute);
 app.use('/api/dogs',DogRoute);
