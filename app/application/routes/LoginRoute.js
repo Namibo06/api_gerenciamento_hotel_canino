1f3301317eb093cdcd807131d3582b06713b3252
Namibo06 <namibo654@gmail.com>
2024-08-18 21:47:35 -0300

    fix: paths

--- app/application/routes/LoginRoute.js
+++ app/application/routes/LoginRoute.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const LoginController = require('../../application/controllers/LoginController');
+const LoginController = require('../controllers/LoginController');
 const loginController = new LoginController();
-const authenticateToken = require('../../application/middlewares/AuthenticateToken');
+const authenticateToken = require('../middlewares/AuthenticateToken');
 
 router.post('/',(req,res) => loginController.authenticate(req,res));
 router.post('/verifyToken', authenticateToken,(req,res) => loginController.verifyToken(req,res));
