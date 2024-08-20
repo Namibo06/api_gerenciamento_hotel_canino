b0ba7efeecf0f54e2839faa8dea69f0ef1844eb6
Namibo06 <namibo654@gmail.com>
2024-08-20 11:17:54 -0300

    feat: added transaction to feed membership table

--- app/infrastructure/repositories/DogRepository.js
+++ app/infrastructure/repositories/DogRepository.js
@@ -1,4 +1,7 @@
-const Dog = require("../../domain/models/Dog")
+const Dog = require("../../domain/models/Dog");
+const owner = require("../../domain/models/Owner");
+const Owner = require("../../domain/models/Owner");
+const { sequelize } = require("../config/Database");
 
 module.exports = class DogRepository{
     async findAll(){
@@ -9,8 +12,25 @@ module.exports = class DogRepository{
         return await Dog.findByPk(uuid);
     }
 
-    async create(dog){
-        return await Dog.create(dog);
+    async create(dog, owners) {
+        return await sequelize.transaction(async (t) => {
+            const createDog = await Dog.create(dog, {
+                transaction: t
+            });
+    
+            if (owners && owners.length > 0) {
+                const ownersInstances = await Owner.findAll({
+                    where: {
+                        id: owners,
+                    },
+                    transaction: t
+                });
+                await createDog.setDogOwners(ownersInstances, {
+                    transaction: t
+                });
+            }
+            return createDog;
+        });
     }
 
     async update(uuid,dog){
