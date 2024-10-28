0924e4b6ca7f63bef782b8c542bccf4d4512534c
Namibo06 <namibo654@gmail.com>
2024-10-28 01:07:52 -0300

    feat: added associate

--- app/domain/models/Dog.js
+++ app/domain/models/Dog.js
@@ -1,51 +1,51 @@
-const { DataTypes,UUIDV4 } = require('sequelize');
-const {sequelize} = require('../../infrastructure/config/Database');
+const { DataTypes, UUIDV4 } = require('sequelize');
+const { sequelize } = require('../../infrastructure/config/Database');
 
-const dog = sequelize.define('tb_dogs',{
-    id:{
+const dog = sequelize.define('tb_dogs', {
+    id: {
         primaryKey: true,
         type: DataTypes.UUID,
         defaultValue: UUIDV4,
     },
-    name:{
-        type: DataTypes.STRING,
+    name: {
+        type: DataTypes.STRING(15),
         allowNull: false,
     },
-    year:{
+    year: {
         type: DataTypes.INTEGER,
         allowNull: false,
     },
-    color:{
-        type: DataTypes.STRING,
+    color: {
+        type: DataTypes.STRING(30),
         allowNull: false,
     },
-    postage:{
-        type: DataTypes.ENUM('Pequeno','Medio','Grande'),
+    postage: {
+        type: DataTypes.ENUM('PEQUENO', 'MEDIO', 'GRANDE'),
         allowNull: false,
     },
-    race:{
-        type: DataTypes.STRING,
+    race: {
+        type: DataTypes.STRING(25),
         allowNull: false,
     },
-    restrictions:{
-        type: DataTypes.JSON,
-        allowNull: true,
-    },
-    deficiency:{
+    deficiency: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-    },
-    owners:{
-        type: DataTypes.JSON,
-        allowNull: false,
-    },
+    }
 });
 
-dog.associate = function(models){
-    dog.belongsToMany(models.owner,{
+dog.associate = function (models) {
+    dog.belongsToMany(models.owner, { 
         through: 'tb_owner_dogs',
         as: 'dogOwners',
         foreignKey: 'dogId',
+        onDelete: 'CASCADE'
+    });
+
+    dog.belongsToMany(models.restriction, {
+        through: 'tb_restriction_dogs',
+        as: 'dogRestrictions',
+        foreignKey: 'dogId',
+        onDelete: 'CASCADE',
     });
 };
 
