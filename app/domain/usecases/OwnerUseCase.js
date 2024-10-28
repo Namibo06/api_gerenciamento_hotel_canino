ee69e6d1522dd3af14ad8498c527c8cbb8c7f7a7
Namibo06 <namibo654@gmail.com>
2024-10-28 01:09:45 -0300

    feat: added try and catch

--- app/domain/usecases/OwnerUseCase.js
+++ app/domain/usecases/OwnerUseCase.js
@@ -29,52 +29,78 @@ module.exports = class OwnerUseCase{
         if(owner === null){
             return res.status(404).json({message: "Dono não encontrado"});
         }
-
         return owner;
     }
 
     async createOwner(req,res){
-        const { first_name, last_name, cpf, email, phone } = req.body;
+        try{
+            const { first_name, last_name, cpf, email, phone } = req.body;
     
-        validateEmail(res,email);
-        if (res.headersSent) return;
-        validateLength(res,first_name,last_name,cpf,phone);
-        if (res.headersSent) return;
-        validateEmpty(res,first_name,email,phone,cpf);
-        if (res.headersSent) return;
+            validateEmail(res,email);
+            if (res.headersSent) return;
+            validateLength(res,first_name,last_name,cpf,phone);
+            if (res.headersSent) return;
+            validateEmpty(res,first_name,email,phone,cpf);
+            if (res.headersSent) return;
 
-        const existsOwner = await this.OwnerService.cpfExists(cpf);
-        
-        if(existsOwner){
-            return res.status(409).json({message: "CPF já cadastrado"});
+            const emailExists = await this.OwnerService.emailExists(email);
+            if (emailExists) {  
+                return res.status(409).json({ message: "Email já ativo para outro usuário" });
+            }
+
+            const existsOwner = await this.OwnerService.cpfExists(cpf);
+            
+            if(existsOwner){
+                return res.status(409).json({message: "CPF já cadastrado"});
+            }
+
+            const createdOwner = this.OwnerService.create(req.body);
+            return createdOwner;
+        }catch (error) {
+            console.error("Erro:", error);
+            return res.status(500).json({
+                status: "error",
+                message: "Falha ao tentar criar dono",
+            });
         }
-
-        const createdOwner = this.OwnerService.create(req.body);
-        return createdOwner;
+        
     }
 
     async updateOwner(req,res){
-        const { first_name, last_name, cpf, email, phone } = req.body;
+        try{
+            const { first_name, last_name, cpf, email, phone } = req.body;
     
-        const uuid = req.params.uuid;
-        validateFormatUUID(res,uuid);
-        if (res.headersSent) return;
+            const uuid = req.params.uuid;
+            validateFormatUUID(res,uuid);
+            if(res.headersSent) return;
 
-        await this.OwnerService.getByUUID(uuid);
-    
-        validateEmail(res,email);
-        if (res.headersSent) return;
-        validateLength(res,first_name,last_name,cpf,phone);
-        if (res.headersSent) return;
-        validateEmpty(res,first_name,email,phone,cpf);
-        if (res.headersSent) return;
+            const existsOwner = await this.OwnerService.getByUUID(uuid);
+            if(existsOwner === null){
+                return res.status(404).json({message: "Dono não encontrado"});
+            }
 
-        const updatedOwner = await this.OwnerService.update(uuid,req.body);
+            const emailExists = await this.OwnerService.emailExists(email);
+            if (emailExists && emailExists.id !== uuid) {  
+                return res.status(409).json({ message: "Email já ativo para outro usuário" });
+            }
+        
+            validateEmail(res,email);
+            if (res.headersSent) return;
+            validateLength(res,first_name,last_name,cpf,phone);
+            if (res.headersSent) return;
+            validateEmpty(res,first_name,email,phone,cpf);
+            if (res.headersSent) return;
 
-        if(!updatedOwner){
-            return res.status(500).json({message: "Falha ao tentar atualizar"});
+            const updatedOwner = await this.OwnerService.update(uuid,req.body);
+
+            if(!updatedOwner){
+                return res.status(500).json({message: "Falha ao tentar atualizar"});
+            }
+
+            return updatedOwner;
+        }catch (error) {
+            console.log("Error: "+error);
         }
-        return updatedOwner;
     }
 
     async deleteOwner(req,res){
