6a201d95a297a5c7961774924189f993faf6b68c
Namibo06 <namibo654@gmail.com>
2024-10-28 01:10:59 -0300

    feat: dog repository

--- app/infrastructure/repositories/DogRepository.js
+++ app/infrastructure/repositories/DogRepository.js
@@ -1,27 +1,45 @@
 const Dog = require("../../domain/models/Dog");
-const owner = require("../../domain/models/Owner");
+const Restriction = require("../../domain/models/Restriction");
 const Owner = require("../../domain/models/Owner");
 const { sequelize } = require("../config/Database");
 
 module.exports = class DogRepository{
     async findAll(){
-        return await Dog.findAll();
+        const dogs = await Dog.findAll({
+            include: {
+                model: Owner,
+                as: 'dogOwners',
+               /* through: {
+                    attributes: []  // Opcional: se você não quiser retornar os dados da tabela de junção
+                }*/
+            }
+        });
+        return dogs;
     }
 
     async getByUUID(uuid){
-        return await Dog.findByPk(uuid);
+        console.log(uuid);
+        return await Dog.findOne({
+            where:{
+                id: uuid
+            },
+            include: {
+                model: Owner,
+                as: 'dogOwners',
+            }
+        });
     }
 
-    async create(dog, owners) {
+    async create(dog, ownersCpf, restrictionsName) {
         return await sequelize.transaction(async (t) => {
             const createDog = await Dog.create(dog, {
                 transaction: t
             });
     
-            if (owners && owners.length > 0) {
+            if (ownersCpf && ownersCpf.length > 0) {
                 const ownersInstances = await Owner.findAll({
                     where: {
-                        id: owners,
+                        cpf: ownersCpf,
                     },
                     transaction: t
                 });
@@ -29,15 +47,62 @@ module.exports = class DogRepository{
                     transaction: t
                 });
             }
+
+            if (restrictionsName && restrictionsName.length > 0) {
+                const restrictionsInstances = await Restriction.findAll({
+                    where: { type_name: restrictionsName },
+                    transaction: t
+                });
+                await createDog.setDogRestrictions(restrictionsInstances, { 
+                    transaction: t 
+                });
+            }
+
             return createDog;
         });
     }
 
-    async update(uuid,dog){
-        return await Dog.update(dog,{
-            where:{
-                id:uuid
+    async update(uuid,dog, ownersCpf, restrictionsName){
+        return await sequelize.transaction(async (t) => {
+            // Primeiro, encontre a instância do cachorro
+            const updateDog = await Dog.findByPk(uuid, { transaction: t });
+    
+            if (!updateDog) {
+                throw new Error('Cachorro não encontrado');
             }
+    
+            // Em seguida, atualize a instância do cachorro
+            await updateDog.update(dog, { transaction: t });
+    
+            // Se ownersCpf estiver definido, atualize os donos
+            if (ownersCpf && ownersCpf.length > 0) {
+                const ownersInstances = await Owner.findAll({
+                    where: {
+                        cpf: ownersCpf,
+                    },
+                    transaction: t
+                });
+                await updateDog.setDogOwners(ownersInstances, {
+                    transaction: t
+                });
+            }
+    
+            // Se restrictionsName estiver definido, atualize as restrições
+            if (restrictionsName && restrictionsName.length > 0) {
+                const restrictionsInstances = await Restriction.findAll({
+                    where: { type_name: restrictionsName },
+                    transaction: t
+                });
+                await updateDog.setDogRestrictions(restrictionsInstances, { 
+                    transaction: t 
+                });
+            } else {
+                await updateDog.setDogRestrictions([], { 
+                    transaction: t 
+                });
+            }
+    
+            return updateDog;
         });
     }
 
