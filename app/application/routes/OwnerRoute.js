7789870a54ca73b1783dd039177a225c6a05c6d1
Namibo06 <namibo654@gmail.com>
2024-10-28 01:06:05 -0300

    feat: added route delete

--- app/application/routes/OwnerRoute.js
+++ app/application/routes/OwnerRoute.js
@@ -8,7 +8,7 @@ router.get('/', authenticateToken,(req,res) => ownerController.listOwners(req,re
 router.get('/:uuid', authenticateToken,(req,res) => ownerController.getOwnerByUUID(req,res));
 router.post('/', authenticateToken,(req,res) => ownerController.createOwner(req,res));
 router.put('/:uuid', authenticateToken,(req,res) => ownerController.updateOwner(req,res));
-router.delete('/:uuid', authenticateToken,(req,res) => ownerController.deleteUser(req,res));
+router.delete('/:uuid', authenticateToken,(req,res) => ownerController.deleteOwner(req,res));
 
 
 module.exports = router;
