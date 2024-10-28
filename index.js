113fda06d27b9546191ac5b7f09387c8f09e48ef
Namibo06 <namibo654@gmail.com>
2024-10-28 01:04:01 -0300

    feat: added associates and requires

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('swagger-jsdoc');
 const { connectDB,sequelize } = require('./app/infrastructure/config/Database');
+const RestrictionRoute = require('./app/application/routes/RestrictionRoute');
 const UserRoute = require('./app/application/routes/UserRoute');
 const OwnerRoute = require('./app/application/routes/OwnerRoute');
 const DogRoute = require('./app/application/routes/DogRoute');
 const LoginRoute = require('./app/application/routes/LoginRoute');
 const owner = require('./app/domain/models/Owner');
 const dog = require('./app/domain/models/Dog');
+const restriction = require('./app/domain/models/Restriction');
 
 const app = express();
 app.use(express.json());
@@ -55,13 +57,15 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
 
 app.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerDocs));
+app.use('/api/restrictions',RestrictionRoute);
 app.use('/api/users',UserRoute);
 app.use('/api/owners',OwnerRoute);
 app.use('/api/dogs',DogRoute);
 app.use('/api/login',LoginRoute);
 
+dog.associate({ owner, restriction });
 owner.associate({ dog });
-dog.associate({ owner });
+restriction.associate({ dog });
 
 sequelize.sync().then(() => {
     app.listen(port,() => {
