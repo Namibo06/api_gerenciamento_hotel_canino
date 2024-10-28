e780e3ffd7a18120c2d49c94105047f2673b5981
Namibo06 <namibo654@gmail.com>
2024-10-28 01:08:11 -0300

    refactor: changes types

--- app/domain/models/Owner.js
+++ app/domain/models/Owner.js
@@ -8,32 +8,32 @@ const owner = sequelize.define('tb_owners',{
         defaultValue: UUIDV4
     },
     first_name:{
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(15),
         allowNull: false,
     },
     last_name: {
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(20),
         allowNull: true,
     },
     cpf:{
-        type: DataTypes.STRING,
+        type: DataTypes.CHAR(11),
         unique: true,
         allowNull: false,
     },
     email:{
-        type: DataTypes.STRING,
+        type: DataTypes.STRING(120),
         unique: true,
         allowNull: false,
     },
     phone:{
-        type: DataTypes.STRING,
+        type: DataTypes.CHAR(11),
         allowNull: false,
     },
 });
 
-owner.associate = function(models){
-    owner.belongsToMany(models.dog,{
-        through:'tb_owner_dogs',
+owner.associate = function(models) {
+    owner.belongsToMany(models.dog, {
+        through: 'tb_owner_dogs',
         as: 'ownerDogs',
         foreignKey: 'ownerId',
     });
